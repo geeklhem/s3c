89b92f85256085facc503a9f9441ce9a3d1e7a60
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2015-01-21 15:50:52 +0100

    Columns docstring.

--- s3c/columns.py
+++ s3c/columns.py
@@ -2,10 +2,27 @@ import numpy as np
 
 def col_names_checker(col_names,
                       existing,
-                      default=["n","trait_val","trait_var","date","site","species","x","y"]):
-    default_col_names = dict(zip(default,default))
+                      default=["n","trait_val","trait_var",
+                               "date","site","species","x","y"]):
+    """ Verification of columns names dictionnary. 
 
-    ## Verification of columns names
+    Args:
+        col_names (dict): Columns names correspondance if they are not default.
+        existing (list of list): list of columns in the input.
+        default (dict): Default columns names. 
+    
+    Returns:
+        A dict linking the default column name to the actual column name in 
+        the input.
+
+    Raises:
+        ValueError: If a key of the input dict is not in the possible default 
+            values or if a value of the input dict is not in the input columns
+            name list (existing).
+    """
+
+    default_col_names = dict(zip(default,default))
+    
     if col_names is None:
         col_names = default_col_names
     else:
